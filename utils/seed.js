4d77fc999b7abe9ddccfa36dd3d8c06b17ddacd3
Christina R <xsnachannell@gmail.com>
2022-07-02 18:17:26 -0700

    udpates package.json with script to run seed. updates data.js and seed.js to connect to database and seed it with users

---
+++ utils/seed.js
@@ -0,0 +1,20 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+const  { users } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log("You are connected to database");
+
+    await User.deleteMany({});
+
+//shouldn't need a delete all thoughts because they should be deleted with all users being deleted
+
+    await User.collection.insertMany(users);
+
+    console.table(users);
+    console.info("Seeding Complete!");
+    process.exit(0);
+});
